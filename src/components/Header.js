f5339c248c82f23903e949f398fb117f1d104ee0
as.orehov <as.orehov@tensor.ru>
2021-03-31 10:01:02 +0300

    ''

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.mintformations.co.uk/blog/wp-content/uploads/2020/05/shutterstock_583717939.jpg'
+                 alt='My logo'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src='https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png'
+                 alt='New img'/>
+        </div>
+    )
+};
+
+export default Profile;
