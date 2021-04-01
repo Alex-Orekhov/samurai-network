12ce18f95bde57147ca79df53042934386106acf
as.orehov <as.orehov@tensor.ru>
2021-04-01 19:12:51 +0300

    CSS modules

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://www.mintformations.co.uk/blog/wp-content/uploads/2020/05/shutterstock_583717939.jpg'
                  alt='My logo'/>
         </header>
     )
-}
+};
 
 export default Header;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
-            <img src='https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png'
-                 alt='New img'/>
+        <div className={s.content}>
+            <div>
+                <img src='https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png'
+                     alt='New img'/>
+            </div>
+            <div> Avatar + description</div>
+            <div> My posts</div>
+            <div> New post </div>
+            <div className={s.posts}>
+                <div className={s.item}>
+                    Post 1
+                </div>
+                <div className={s.item}>
+                    Post 2
+                </div>
+            </div>
         </div>
     )
 };
