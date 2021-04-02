823c4506ef6e359f738e5fd16a7079dbc1518424
as.orehov <as.orehov@tensor.ru>
2021-04-02 19:04:45 +0300

    Add Routing

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+
+
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navigation/>
-            <Profile/>
-        </div>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header />
+                <Navigation />
+                <div className='app-wrapper-content'>
+                    <Route path={'/dialogs'} component={Dialogs}/>
+                    <Route path={'/profile'} component={Profile}/>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png'
                      alt='New img'/>
