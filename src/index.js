91612650c6c765c5154deca1da9cf507b5ad5499
as.orehov <as.orehov@tensor.ru>
2021-04-16 10:46:30 +0300

    subscribe-observe

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import './index.css';
+import ReactDOM from "react-dom";
 import reportWebVitals from './reportWebVitals';
 
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import './index.css';
+
+import state, {addPost, subscribe, updateNewPostText} from "./redux/state";
+
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App
+                    state={state}
+                    addPost={addPost}
+                    updateNewPostText={updateNewPostText}/>
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+
+};
 
 let state = {
     profilePage: {
@@ -27,7 +29,7 @@ let state = {
     }
 }
 
-export let addPost = () => {
+export const addPost = () => {
 
     let newPost = {
         id: 5,
@@ -40,10 +42,14 @@ export let addPost = () => {
     rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
 
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
-}
+};
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
+};
 
 export default state;
