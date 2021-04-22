c506f9edee97705081ddff12336e645c4865b298
as.orehov <as.orehov@mail.ru>
2021-04-22 23:45:28 +0300

    Added dispatch() in store

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -8,13 +8,12 @@ const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        props.addPost();
+    function addPost() {
+        props.dispatch({ type: 'ADD-POST' });
     }
 
     let onPostChange = () => {
-        let text = newPostElement.current.value;
-        props.updateNewPostText(text);
+        props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: newPostElement.current.value});
     }
 
     return (
--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ let rerenderEntireTree = () => {
             <BrowserRouter>
                 <App
                     state={store.getState()}
-                    addPost={store.addPost.bind(store)}
-                    updateNewPostText={store.updateNewPostText.bind(store)}/>
+                    dispatch={store.dispatch.bind(store)}/>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
